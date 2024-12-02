634c65126b7a83650b2ae31e93d47841e8fcca54
Sakshi Khatod <sakshi.khatod1998@gmail.com>
2024-12-02 23:28:13 +0530

    add subscription class

---
+++ microservices/subscription.py
@@ -0,0 +1,13 @@
+class Subscription:
+
+    def __init__(self) -> None:
+        ___no_of_month=0
+        __category=None
+        __price=0
+
+    def getprice():
+        pass
+
+    def get_validity():
+        pass
+        
